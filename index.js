af6f9e7173099d059cccfe2eb3e69acda3573e2a
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-05-30 11:23:50 +0600

    fixed problem in api server

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     const database = client.db("imadrasah");
     const coursesCollection = database.collection("courses");
@@ -264,7 +264,7 @@ async function run() {
       })
 
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     
